cb5fbf60871e829101d326ecda2f1452824506ab
Robert Berry <rjberry@gmail.com>
2013-01-02 21:58:20 +0000

    Refactored test to be based on example.

---
+++ src/main/scala/com/github/robertberry/fsm/deterministic/examples/EndsWithZero.scala
@@ -0,0 +1,20 @@
+package com.github.robertberry.fsm.deterministic.examples
+
+import com.github.robertberry.fsm.deterministic._
+
+object EndsWithZero {
+  val q1 = State("q1")
+  val q2 = State("q2")
+
+  val states = Set[State](q1, q2)
+  val finishStates = Set[State](q2)
+
+  def transition(state: State, character: BinaryAlphabet): State = (state, character) match {
+    case (`q1`, Zero) => q2
+    case (`q1`, One) => q1
+    case (`q2`, Zero) => q2
+    case (`q2`, One) => q1
+  }
+
+  val machine = new DeterministicFSM[BinaryAlphabet](states, transition, q1, finishStates)
+}
--- src/test/scala/com/github/robertberry/fsm/deterministic/DeterministicFSMTest.scala
+++ src/test/scala/com/github/robertberry/fsm/deterministic/DeterministicFSMTest.scala
@@ -1,5 +1,6 @@
 package com.github.robertberry.fsm.deterministic
 
+import examples.EndsWithZero
 import org.scalatest.FlatSpec
 
 /**
@@ -7,28 +8,11 @@ import org.scalatest.FlatSpec
  */
 class DeterministicFSMTest extends FlatSpec {
   "A Deterministic Finite State Machine" should "only allow start states that are part of its set of states" in {
-    val q1 = State("q1")
-    val q2 = State("q2")
-    val q3 = State("q3")
-
-    val states = Set[State](q1, q2, q3)
-    val finishStates = Set[State](q3)
-
-    val alphabet = "01".toSet
-
-    def transition(state: State, character: BinaryAlphabet): State = (state, character) match {
-      case (`q1`, Zero) => q2
-      case (`q1`, One) => q3
-      case (`q2`, Zero) => q3
-      case (`q2`, One) => q2
-      case (`q3`, Zero) => q1
-      case (`q3`, One) => q2
-    }
-
-    val machine = new DeterministicFSM[BinaryAlphabet](states, transition, q1, finishStates)
+    val endsWithZero = EndsWithZero.machine
 
     intercept[IllegalArgumentException] {
-      val badMachine = new DeterministicFSM[BinaryAlphabet](states, transition, State("q4"), finishStates)
+      val badMachine = new DeterministicFSM[BinaryAlphabet](endsWithZero.states, endsWithZero.transition,
+        State("q4"), endsWithZero.finishStates)
     }
   }
 }
